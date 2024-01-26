5a11564dfe9228ed3bfb9bd52a995f5247288509
Matographo <Matographo@gmail.com>
2024-01-26 03:08:00 +0100

    ZipManager klasse hinzugefügt
    ZipManager function: zip, unzip.

--- src/main/java/de/multitool/Main.java
+++ src/main/java/de/multitool/Main.java
@@ -1,17 +1,7 @@
 package de.multitool;
 
-import javafx.application.Platform;
-import javafx.geometry.Pos;
-import org.controlsfx.control.Notifications;
-
 public class Main {
     public static void main(String[] args) {
-        Platform.runLater(() -> {
-            Notifications notificationTest = Notifications.create();
-            notificationTest.position(Pos.BASELINE_RIGHT);
-            notificationTest.title("Hallo Welt");
-            notificationTest.text("Schöner Test");
-            notificationTest.show();
-        });
+        ZipManager.unzip("/home/leodora/Downloads/Hure.zip", "/home/leodora/Downloads");
     }
 }
